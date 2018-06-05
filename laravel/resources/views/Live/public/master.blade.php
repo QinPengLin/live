dc983d54a5992047f369598bd893109558a6500d
mymacbook <1017962047@qq.com>
2018-06-06 03:30:40 +0800

    live

--- laravel/resources/views/Live/public/master.blade.php
+++ laravel/resources/views/Live/public/master.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     <title>Live</title>
-    
+
+ 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <link href="{{URL::asset('/live/css/bootstrap.min.css')}}" rel="stylesheet" />
