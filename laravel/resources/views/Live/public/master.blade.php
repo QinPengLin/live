66ceb1b9fd036f716192098dc1f2449f40bf6a20
mymacbook <1017962047@qq.com>
2018-06-06 03:14:36 +0800

    live

--- laravel/resources/views/Live/public/master.blade.php
+++ laravel/resources/views/Live/public/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <link rel="icon" type="image/png" href="{{URL::asset('favicon.ico')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
 
     <title>Live</title>
 
