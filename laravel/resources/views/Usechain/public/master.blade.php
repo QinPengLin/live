9124170fdd5512062ac44ca4f34dd5b322f6a4b4
mymacbook <1017962047@qq.com>
2018-06-12 16:53:55 +0800

    Usechain

--- laravel/resources/views/Usechain/public/master.blade.php
+++ laravel/resources/views/Usechain/public/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no," />
-    <title>Usechain——全球首个身份镜像区块链</title>
+    <title>6月16号 Usechain 杭州站路演报名预约登记</title>
     <link rel="stylesheet" type="text/css" href="{{URL::asset('/usechain/css/usechain_mian.css')}}" />
     <script type="text/javascript" src="{{URL::asset('/usechain/js/jquery-1.4.2.min.js')}}"></script>
     @yield('after-styles-end')
