77ca39855cbf7ad9b8570312b7c43b8df09e42b7
mymacbook <1017962047@qq.com>
2018-06-28 17:19:04 +0800

    bet365

--- laravel/app/Http/Controllers/Live/IndexController.php
+++ laravel/app/Http/Controllers/Live/IndexController.php
@@ -32,7 +32,6 @@ class IndexController extends Controller
             $rearr['success']=1;
             json_decode($redata,true)['asian_lines']?$asian_lines=json_decode($redata,true)['asian_lines']:$asian_lines=null;
             json_decode($redata_main,true)['main']?$main=json_decode($redata_main,true)['main']:$main=null;
-            $asian_lines
             $rearr['results']=array('FI'=>$dat['key'],'asian_lines'=>$asian_lines,'main'=>$main);
             return json_encode($rearr);
         }else{
